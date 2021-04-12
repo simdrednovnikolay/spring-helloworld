89d2be9913a5e228e8ef3d0a15dbabc9f847d826
Nikolay Rednov <red.niko@yandex.ru>
2021-04-12 15:01:47 +0400

    Bean - done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,18 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        boolean compare = bean == bean2;
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        boolean cmpare2 = cat == cat2;
+
+        System.out.println(compare);
+        System.out.println(cmpare2);
     }
 }
